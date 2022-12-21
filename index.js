d963fb542e9422f62792ad43d5cce299dfd65c1e
NathanMad <nathan@internit.com.br>
2022-12-21 17:17:56 -0300

    layout updade

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    for(let btn of document.querySelectorAll('[data-target]')) {
+    for (let btn of document.querySelectorAll('[data-target]')) {
         btn.addEventListener('click', (e) => {
-            if(document.querySelector(btn.dataset.target).dataset.state == 'false') {
+            if (document.querySelector(btn.dataset.target).dataset.state == 'false') {
                 document.querySelector(btn.dataset.target).dataset.state = 'true';
-                if( btn.querySelector('.fa-caret-down')) {
+                if (btn.querySelector('.fa-caret-down')) {
                     btn.querySelector('.fa-caret-down').classList.add('fa-caret-up');
                     btn.querySelector('.fa-caret-down').classList.remove('fa-caret-down');
                 }
             } else {
                 document.querySelector(btn.dataset.target).dataset.state = 'false';
-                if( btn.querySelector('.fa-caret-up')) {
+                if (btn.querySelector('.fa-caret-up')) {
                     btn.querySelector('.fa-caret-up').classList.add('fa-caret-down');
                     btn.querySelector('.fa-caret-up').classList.remove('fa-caret-up');
                 }
@@ -20,7 +20,47 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
+    var swiperTexto = new Swiper('.swiper-text', {
+        // Optional parameters
+        // loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
+    var swiperCard = new Swiper('.swiper-card', {
+        // Optional parameters
+        // loop: true,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        // scrollbar: {
+        //     el: '.swiper-scrollbar',
+        // },
+    });
+    var swiperGellery = new Swiper('.swiper-gallery', {
         // Optional parameters
         // loop: true,
       
